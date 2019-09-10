a6cd49dd2ce27e9b38421f3a6033954d708520d7
Rémy Kaloustian <remy.kaloustian@gmail.com>
2019-09-10 15:34:28 +0200

    Removing license header

--- src/designpatternsshowcase/strategy/StrategyMain.java
+++ src/designpatternsshowcase/strategy/StrategyMain.java
@@ -1,14 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package designpatternsshowcase.strategy;
 
 import designpatternsshowcase.Utils;
+import designpatternsshowcase.strategy.eat.EatGrainBehavior;
 import designpatternsshowcase.strategy.eat.EatMeatBehavior;
+import designpatternsshowcase.strategy.log.LogDotsBehavior;
 import designpatternsshowcase.strategy.log.LogStarsBehavior;
+import designpatternsshowcase.strategy.move.MoveFlyBehavior;
 import designpatternsshowcase.strategy.move.MoveWalkBehavior;
+import designpatternsshowcase.strategy.speak.SpeakMeowBehavior;
 import designpatternsshowcase.strategy.speak.SpeakRoarBehavior;
 
 /**
@@ -20,7 +20,11 @@ public class StrategyMain {
         Utils.print("Creating Cyborg animals");
         
         CyborgAnimal EagleTiger = new CyborgAnimal("EagleTiger", 
-                new MoveWalkBehavior(), new EatMeatBehavior(), 
+                new MoveFlyBehavior(), new EatMeatBehavior(), 
                 new SpeakRoarBehavior(), new LogStarsBehavior());
+        
+        CyborgAnimal CatPenguin = new CyborgAnimal("CatPenguin", 
+                new MoveWalkBehavior(), new EatGrainBehavior(),
+                new SpeakMeowBehavior(), new LogDotsBehavior());
     }
 }
